db65d5e0785efee0e11aa6c315726003f086c337
BestIsReaLoL <71178802+BestIsReaLoL@users.noreply.github.com>
2022-06-05 16:27:28 +0300

    Add files via upload

---
+++ komutlar/[Rowie]-vip-al.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const rdb = require('quick.db');
+const moment = require('moment');
+exports.run = async (client, message, args) => {
+let vipal = '971473784169168968' //VİP ROLÜ
+if(!["971473784261443689"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) 
+  return message.channel.send(`Bu komutu kullanabilmek için ayarlanan kayıt yetkisine sahip olmalısınız!`).then(x => x.delete({timeout: 5000}));
+  let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+  if (!member) return message.channel.send('Bir üye etiketlemelisin.').then(x => x.delete({timeout: 5000}));
+ member.roles.remove(vipal)
+  let embed = new Discord.MessageEmbed()
+  .setColor("2d3136")
+  .setDescription(`${member} kişisi'den Artık <@&971473784169168968> Rolü Alınmıştır.`)
+  .setTimestamp()
+  message.react('✅')
+//message.react(client.emojiler.onay).catch();
+message.channel.send(embed).then(x => x.delete({timeout: 5000}));
+} 
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['vip-al'],
+  permLevel: 0
+}
+exports.help = {
+  name: 'vip-al',
+  description: "Belirtilen üyeye kayıtsız rolü verir",
+  usage: 'vip-al @kişi'
+}
